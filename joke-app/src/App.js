6f88846bcdd0c67a911477eabd66ef7f4c3bddc3
Snipez9492 <waleibk92@gmail.com>
2022-04-06 14:48:39 -0400

    Second Commit

--- joke-app/src/App.js
+++ joke-app/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+const axios = require('axios');
 
 function App() {
+
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.icndb.com/jokes/random?firstName=Olawale&lastName=Orimogunje').then(function(response){
+    //console.log(response);
+    //setJoke(data.value.joke);
+    setJoke(response.data.value.joke);
+    }).catch(function (error) {
+    
+      console.log(error);
+    })
+    .then(function () {
+  });
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1>The Joke Generator</h1>
+            <h2>{joke}</h2>
+        <button>Generator Joke</button>
+      </center>
     </div>
   );
 }
